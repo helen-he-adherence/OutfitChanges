0690a944ca228846bda2a09041c7cac0e5d59f0a
helenhe <3456268174@qq.com>
2025-12-10 23:39:16 +0800

    2025-12-10 over

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.12.0")
 
     // ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
